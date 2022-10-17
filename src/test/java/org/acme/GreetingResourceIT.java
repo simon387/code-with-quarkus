dca2b76c1b94845eb516ba771f5092a0a7fdfe4f
Simone Celia <simone.celia@simonecelia.it>
2022-10-17 12:54:05 +0200

    tabs

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -2,7 +2,8 @@ package org.acme;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
+
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
+	// Execute the same tests but in packaged mode.
 }
